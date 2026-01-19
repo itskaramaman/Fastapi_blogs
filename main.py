9c26190b05ba701a2f19761903078b3d9c28d7f2
Karamjeet Singh <singhkaramjeetapple.com>
2026-01-19 13:44:49 -0800

    post model and user apis (create, get_user)

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI, HTTPException, Request, status
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from schemas import PostCreate, PostResponse
+import models
+from database import Base, engine, get_db
+from schemas import PostCreate, PostResponse, UserCreate, UserResponse
+
+
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory="media"), name="media")
 templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
@@ -43,6 +53,44 @@ def post_page(request: Request, post_id: int):
             return templates.TemplateResponse(request, "post.html", {"post": post, "title": title})
     
     return templates.TemplateResponse(request, 'error.html', {"status_code": status.HTTP_404_NOT_FOUND, "message": "Post not found"})
+
+
+@app.post("/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.username == user.username))
+
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+    
+    result = db.execute(select(models.User).where(models.User.email == user.email))
+
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
+    
+    new_user = models.User(
+        username=user.username,
+        email=user.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@app.get("/api/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = db.execute(select(models.User).where(id=user_id))
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not Found")
+    
+    return user
 
 @app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
--- schemas.py
+++ schemas.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    image_str: int | None
+    image_path: str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -8,11 +25,13 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    pass 
+    user_id: int 
 
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
